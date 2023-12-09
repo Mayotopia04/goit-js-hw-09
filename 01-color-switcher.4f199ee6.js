98032379571558780f3f18f423039cf46a1419e8
Mayotopia04 <134679840+Mayotopia04@users.noreply.github.com>
2023-12-09 11:39:28 +0000

    Deploying to gh-pages from @ Mayotopia04/goit-js-hw-09@326c0f6a2bd5ac42b8f77e9d6769542ce5f34f21 🚀

---
+++ 01-color-switcher.4f199ee6.js
@@ -0,0 +1,2 @@
+let t=null;const e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(()=>{e.disabled=!0,t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),a.addEventListener("click",(()=>{clearInterval(t),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.4f199ee6.js.map
